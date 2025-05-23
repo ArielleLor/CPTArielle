488c21f32136deac90f37059c25c8bde864c5daf
ArielleLor <Arielle.lor26@ycdsbk12.ca>
2025-05-23 10:52:35 -0400

    added more prints

--- repotest.java
+++ repotest.java
@@ -4,6 +4,8 @@ public class repotest{
 	public static void main(String[] args){
 	Console con = new Console();
 	con.println("Hello World");
+	con.println("TGIF");
+	con.println("CPT Time!");
 
 	}
 }
