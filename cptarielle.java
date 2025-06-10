af888957a2c959c95e303b699bb1d70c2d04346a
ArielleLor <Arielle.lor26@ycdsbk12.ca>
2025-06-10 09:38:15 -0400

    Making interface, made quit page, worked on quiz questions

--- cptarielle.java
+++ cptarielle.java
@@ -5,5 +5,38 @@ public class cptarielle{
 		Console con = new Console();
 		con.println("Hello");
 		
+		String strMenu = " ";
+		//Format of question
+		//Question - String
+		//Choices - String
+		//Answer - String
+		String strQ;
+		String strC;
+		String strA;
+		
+		//Menu Items
+		con.println("(P)lay Game - 1");
+		con.println("(L)eaderboard - 2");
+		con.println("(A)dd Quiz - 3");
+		con.println("(Q)uit - 4");
+		strMenu = con.readLine();
+
+		
+		//The If statement is to Quit
+		if(strMenu.equalsIgnoreCase("Q")) {
+			con.closeConsole();
+		}
+		
+		//To play the game
+		if(strMenu.equalsIgnoreCase("P")) {
+		//Opening quizzes.txt
+		//Opening this file shows all quizzes availible
+		TextInputFile quizzes = new TextInputFile("quizzes.txt");
+		while(quizzes.eof() != true){
+		}
+	}
+
+				
+
 	}
 }	
